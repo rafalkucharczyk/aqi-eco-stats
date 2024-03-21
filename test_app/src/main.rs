3f50736e9a52f614cce813b20da8e900d306ab37
Rafal Kucharczyk <rafal@sophicode.com>
2024-03-21 20:42:17 +0100

    First tries with cargo-lambda
    Unit tests are still missing

--- test_app/src/main.rs
+++ test_app/src/main.rs
@@ -1,6 +1,15 @@
-#[tokio::main]
-async fn main() {
-    static BASE_URL: &str = "https://trzebnica.aqi.eco/pl";
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde_json::{json, Value};
 
-    println!("{:?}", fetch_data::get_weekly_stats(BASE_URL).await);
+async fn run_lambda(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let (event, _context) = event.into_parts();
+
+    let base_url = event["url"].as_str().unwrap_or_default();
+    let result = fetch_data::get_weekly_stats(base_url).await;
+    Ok(json!(result))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(service_fn(run_lambda)).await
 }
