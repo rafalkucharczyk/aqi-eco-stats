3c4f2030d45afaf363f0a352c44b9f39d9e8e839
Rafal Kucharczyk <rafal@sophicode.com>
2024-03-23 12:56:58 +0100

    Mock get_weekly stats for lambda testing
    Reorganize fetch_data library into core and utils modules,
    so that core can be mocked.

--- fetch_data/src/lib.rs
+++ fetch_data/src/lib.rs
@@ -1,125 +1,134 @@
-mod json_structs;
+pub mod json_structs;
 
-use crate::json_structs::deserializable::{parse_json, Location, SensorData};
-use crate::json_structs::serializable::{LocationMinMax, MeasurementMinMax, WeeklyStats};
+use mockall::automock;
 
-use std::cmp::Ordering;
+mod utils {
+    use crate::json_structs::deserializable::{parse_json, Location};
+    use std::cmp::Ordering;
 
-#[derive(Clone)]
-struct RawSensorData {
-    name: String,
-    json: String,
-}
+    #[derive(Clone)]
+    pub struct RawSensorData {
+        pub name: String,
+        pub json: String,
+    }
 
-async fn get_url_text(url: &str) -> Result<String, reqwest::Error> {
-    println!("{}", url);
-    let body = reqwest::get(url).await?.text().await?;
+    async fn get_url_text(url: &str) -> Result<String, reqwest::Error> {
+        println!("{}", url);
+        let body = reqwest::get(url).await?.text().await?;
 
-    Ok(body)
-}
+        Ok(body)
+    }
 
-async fn get_locations(base_url: &str) -> Vec<Location> {
-    let data = get_url_text(format!("{}/map/data.json", base_url).as_str())
-        .await
-        .unwrap_or_default();
-    parse_json::<Vec<Location>>(&data)
-}
+    async fn get_locations(base_url: &str) -> Vec<Location> {
+        let data = get_url_text(format!("{}/map/data.json", base_url).as_str())
+            .await
+            .unwrap_or_default();
+        parse_json::<Vec<Location>>(&data)
+    }
 
-async fn get_sensor_raw_data(base_url: &str) -> Vec<RawSensorData> {
-    let tasks = get_locations(base_url).await.into_iter().map(|x| {
-        let url = String::from(base_url);
-        tokio::spawn(async move {
-            RawSensorData {
-                json: get_url_text(
-                    format!(
-                        "{}{}/graph_data.json?type=pm&range=week&ma_h=24",
-                        url, x.path
+    pub async fn get_sensor_raw_data(base_url: &str) -> Vec<RawSensorData> {
+        let tasks = get_locations(base_url).await.into_iter().map(|x| {
+            let url = String::from(base_url);
+            tokio::spawn(async move {
+                RawSensorData {
+                    json: get_url_text(
+                        format!(
+                            "{}{}/graph_data.json?type=pm&range=week&ma_h=24",
+                            url, x.path
+                        )
+                        .as_str(),
                     )
-                    .as_str(),
-                )
-                .await
-                .unwrap(),
-                name: x.description,
-            }
-        })
-    });
+                    .await
+                    .unwrap(),
+                    name: x.description,
+                }
+            })
+        });
 
-    futures::future::join_all(tasks)
-        .await
-        .iter()
-        .map(|x| x.as_ref().unwrap().clone())
-        .collect()
-}
-
-fn map_measurements(
-    series: &std::collections::HashMap<u32, Option<f32>>,
-) -> impl Iterator<Item = f32> + '_ {
-    series
-        .iter()
-        .filter(|x| x.1.is_some())
-        .map(|x| x.1.unwrap_or_default())
-}
-
-fn get_min(series: &std::collections::HashMap<u32, Option<f32>>) -> f32 {
-    map_measurements(series)
-        .min_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
-        .unwrap_or_default()
-}
-
-fn get_max(series: &std::collections::HashMap<u32, Option<f32>>) -> f32 {
-    map_measurements(series)
-        .max_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
-        .unwrap_or_default()
-}
-
-pub async fn get_weekly_stats(base_url: &str) -> WeeklyStats {
-    let sensors_raw_data = get_sensor_raw_data(base_url).await;
-
-    let locations: Vec<LocationMinMax> = sensors_raw_data
-        .iter()
-        .filter_map(|x| {
-            let sensor_data = &parse_json::<SensorData>(&x.json);
-            let data = &sensor_data.data;
-
-            if sensor_data != &SensorData::default() {
-                Some(LocationMinMax {
-                    name: String::from(&x.name),
-                    start_date: sensor_data.start,
-                    end_date: sensor_data.end,
-                    pm25: MeasurementMinMax {
-                        min: get_min(&data.pm25),
-                        max: get_max(&data.pm25),
-                    },
-                    pm10: MeasurementMinMax {
-                        min: get_min(&data.pm10),
-                        max: get_max(&data.pm10),
-                    },
-                })
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    WeeklyStats {
-        start: locations
+        futures::future::join_all(tasks)
+            .await
             .iter()
-            .min_by_key(|x| x.start_date)
-            .unwrap_or(&LocationMinMax::default())
-            .start_date,
-        end: locations
+            .map(|x| x.as_ref().unwrap().clone())
+            .collect()
+    }
+
+    fn map_measurements(
+        series: &std::collections::HashMap<u32, Option<f32>>,
+    ) -> impl Iterator<Item = f32> + '_ {
+        series
             .iter()
-            .max_by_key(|x| x.end_date)
-            .unwrap_or(&LocationMinMax::default())
-            .end_date,
-        locations,
+            .filter(|x| x.1.is_some())
+            .map(|x| x.1.unwrap_or_default())
+    }
+
+    pub fn get_min(series: &std::collections::HashMap<u32, Option<f32>>) -> f32 {
+        map_measurements(series)
+            .min_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
+            .unwrap_or_default()
+    }
+
+    pub fn get_max(series: &std::collections::HashMap<u32, Option<f32>>) -> f32 {
+        map_measurements(series)
+            .max_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
+            .unwrap_or_default()
+    }
+}
+
+#[automock]
+pub mod core {
+    use crate::json_structs::deserializable::{parse_json, SensorData};
+    use crate::json_structs::serializable::{LocationMinMax, MeasurementMinMax, WeeklyStats};
+    use crate::utils::{get_max, get_min, get_sensor_raw_data};
+
+    pub async fn get_weekly_stats(base_url: &str) -> WeeklyStats {
+        let sensors_raw_data = get_sensor_raw_data(base_url).await;
+
+        let locations: Vec<LocationMinMax> = sensors_raw_data
+            .iter()
+            .filter_map(|x| {
+                let sensor_data = &parse_json::<SensorData>(&x.json);
+                let data = &sensor_data.data;
+
+                if sensor_data != &SensorData::default() {
+                    Some(LocationMinMax {
+                        name: String::from(&x.name),
+                        start_date: sensor_data.start,
+                        end_date: sensor_data.end,
+                        pm25: MeasurementMinMax {
+                            min: get_min(&data.pm25),
+                            max: get_max(&data.pm25),
+                        },
+                        pm10: MeasurementMinMax {
+                            min: get_min(&data.pm10),
+                            max: get_max(&data.pm10),
+                        },
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        WeeklyStats {
+            start: locations
+                .iter()
+                .min_by_key(|x| x.start_date)
+                .unwrap_or(&LocationMinMax::default())
+                .start_date,
+            end: locations
+                .iter()
+                .max_by_key(|x| x.end_date)
+                .unwrap_or(&LocationMinMax::default())
+                .end_date,
+            locations,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use mockito;
+    use super::core::*;
+    use crate::json_structs::serializable::WeeklyStats;
     use serde_json::json;
 
     #[tokio::test]
--- test_app/src/main.rs
+++ test_app/src/main.rs
@@ -1,15 +1,70 @@
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde_json::{json, Value};
 
+#[cfg(not(test))]
+use fetch_data::core::get_weekly_stats;
+#[cfg(test)]
+use fetch_data::mock_core::get_weekly_stats;
+
 async fn run_lambda(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let (event, _context) = event.into_parts();
 
-    let base_url = event["url"].as_str().unwrap_or_default();
-    let result = fetch_data::get_weekly_stats(base_url).await;
+    let base_url = event.get("url").ok_or("no url")?;
+    let result = get_weekly_stats(base_url.as_str().unwrap()).await;
     Ok(json!(result))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     lambda_runtime::run(service_fn(run_lambda)).await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fetch_data::json_structs::serializable::WeeklyStats;
+    use mockall::predicate;
+    use serial_test::serial;
+
+    #[serial]
+    #[tokio::test]
+    async fn run_lambda_when_url_is_defined_then_returns_get_weekly_stats_result() {
+        let ctx = fetch_data::mock_core::get_weekly_stats_context();
+        ctx.expect()
+            .with(predicate::eq("foo"))
+            .times(1)
+            .returning(|_url| WeeklyStats {
+                start: 5,
+                ..WeeklyStats::default()
+            });
+
+        let r = run_lambda(LambdaEvent::new(
+            json!({"url": "foo"}),
+            lambda_runtime::Context::default(),
+        ))
+        .await;
+
+        assert_eq!(
+            r.unwrap(),
+            json!(WeeklyStats {
+                start: 5,
+                ..WeeklyStats::default()
+            })
+        )
+    }
+
+    #[serial]
+    #[tokio::test]
+    async fn run_lambda_when_url_is_missing_then_returns_error() {
+        let ctx = fetch_data::mock_core::get_weekly_stats_context();
+        ctx.expect().times(0);
+
+        let r = run_lambda(LambdaEvent::new(
+            json!({}),
+            lambda_runtime::Context::default(),
+        ))
+        .await;
+
+        assert_eq!(r.err().unwrap().to_string(), "no url");
+    }
 }
