2c26263d5a00548d1a36d1199891d5348052e3ce
Rafal Kucharczyk <rafal@sophicode.com>
2024-04-23 09:58:54 +0200

    Restructure project
    Move lambda to a separate `backend` subdirectory, get rid of
    `test_app` name in favor of more descriptive `lambda_fetch_and_store`.
    Rename CloudFormation stack too.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -x
 
+PROJECT_NAME=aqi-eco-stats
 TYPE=dev
-STACK_NAME=app-$TYPE-stack
+STACK_NAME=$PROJECT_NAME-$TYPE-stack
 ACTION=create
+
+cd backend
 
 cargo lambda build --release --arm64 --output-format zip
 CODE_HASH=$(unzip -p target/lambda/test_app/bootstrap.zip bootstrap | openssl dgst -md5 | cut -f2 -d' ')
 TARGET_FILE=fetch-and-store/bootstrap-$CODE_HASH.zip
-S3_PATH=s3://aqi-eco-stats-$TYPE-lambda-bucket/$TARGET_FILE
+S3_PATH=s3://$PROJECT_NAME-$TYPE-lambda-bucket/$TARGET_FILE
 
 if ! aws s3 ls $S3_PATH; then
-     aws s3 cp ./target/lambda/test_app/bootstrap.zip $S3_PATH
+     aws s3 cp ./target/lambda/lambda_fetch_and_store/bootstrap.zip $S3_PATH
 fi
 
 if aws cloudformation describe-stacks --stack-name $STACK_NAME --no-cli-pager; then
@@ -19,7 +22,7 @@ fi
 
 aws cloudformation $ACTION-stack \
     --stack-name $STACK_NAME \
-    --template-body file://infra/infra.yml \
+    --template-body file://../cloudformation/backend.yml \
     --parameters ParameterKey=EnvironmentType,ParameterValue=$TYPE  \
     --parameters ParameterKey=FetchAndStoreLambdaFile,ParameterValue=$TARGET_FILE  \
     --capabilities CAPABILITY_IAM \
